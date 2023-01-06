ca13d564f9cbc65d6ac9ae615e45344ddee021f6
harvend04 <114313647+harvend04@users.noreply.github.com>
2023-01-05 23:33:49 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { /* 1ST QUESTION
+            double inc;
+            Console.Write($"Enter the start cm value: ");
+            double cm = Convert.ToDouble(Console.ReadLine());
+            Console.Write($"Enter the end cm value: ");
+            double cm1 = Convert.ToDouble(Console.ReadLine());
+
+            inc = 0.39;
+            Console.WriteLine($"cm\tIn");
+            do
+            {
+                double outp = cm * inc;
+                Console.WriteLine($"{cm}\t{outp:F2}");
+
+                cm += 5;
+                
+                
+                
+
+
+            }
+            while (cm<=cm1);
+            */
+
+            //2ND QUESTION
+            /*
+            int val;
+            Console.Write($"Enter start value: ");
+            val = Convert.ToInt32(Console.ReadLine());
+
+            while (val<=100)
+            {
+                if (val <= 100 )
+                {
+                    Console.Write($"{val} ");
+                }
+                val +=7;
+
+            }
+            */
+            //3RD QUESTION
+
+            /*
+            double target, value, attempt;
+            Console.Write($"Enter target value: ");
+            target= Convert.ToDouble(Console.ReadLine());
+            attempt = 0;
+
+            for (;target>0; target= target- value/2)
+            {
+                Console.Write($"Enter #{attempt+1}: ");
+                value = Convert.ToDouble(Console.ReadLine());
+                attempt++;
+ 
+            }
+            Console.WriteLine($"It took {attempt} inputs to reduce the target to {target}");
+
+            */
+            //4th question
+            /*
+            Console.WriteLine($"x\t2\t3x\t-2x^2\ty");
+            Console.WriteLine($"----\t----\t-----\t----\t----");
+            double x = 1.25;
+
+
+            for (int i = 0; i <=17; i++)
+            {
+                
+                double x1 = x * -1;
+                double x2 = x1 * 3;
+                double x3 = -2 * Math.Pow(x, 2);
+                double y = 2 + x2 + x3;
+                Console.WriteLine($"{x1:F2}\t{2:F2}\t{x2:F2}\t{x3:F2}\t{y:F2}");
+                x -= 0.25;
+
+            }
+            */
+            
+            //QUESTION 5 
+            
+            int row, column;
+            Console.Write($"Number of columns: ");
+            column = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Number of rows: ");
+            row = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <=row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if ((j+i) % 7 == 0)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+            /*
+            int row, column;
+            Console.Write($"Number of columns: ");
+            column = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Number of rows: ");
+            row = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= row; i++)
+            {
+                for (int j=1; j <= column; j++)
+                {
+                    if (i == 1|| j==1||j%7==0||i%6==0)
+                    {
+                        Console.Write("+");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+            */
+
+
+        }
+    }
+}
